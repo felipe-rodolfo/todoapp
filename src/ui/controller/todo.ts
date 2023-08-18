71e943b05cc53be36612c13f98e0e9b91596ddeb
Felipe Rodolfo <felipewebdeveloper@gmail.com>
2023-08-17 21:30:18 -0300

    GET - HTTP Protocol Integrating from Back to Front with Controllers

---
+++ src/ui/controller/todo.ts
@@ -0,0 +1,12 @@
+async function get() {
+  return fetch("/api/todos")
+      .then(async (response) => {
+        const todosString = await response.text();
+        const todosFromServer = JSON.parse(todosString).todos;
+        return todosFromServer;
+    })
+}
+
+export const todoController = {
+  get,
+}
